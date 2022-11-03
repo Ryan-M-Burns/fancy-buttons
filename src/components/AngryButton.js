d672525e4799fddcecb8598748d7125344f56383
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-03 14:54:59 -0700

    implement onClick handlers

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
-const AngryButton = () => {
-  
+const AngryButton = (props) => {
+
   const [anger, setAnger] = useState(0);
-  
+
+  const handleClick = () => setAnger(anger < 1 ? anger + 0.1 : 0);
+
   return (
-    
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="Angry Button">
+
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} onClick={handleClick} className="Angry Button">
       {anger < 1 && <span>Don't click me too much!</span>}
       {anger > 1 && <span>Rawr!</span>}
     </button>
